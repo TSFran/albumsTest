1792199216a4c3249f934dd06bd0ecffa8786f90
Luis Tupa <luistupa6@gmail.com>
2019-01-08 14:15:28 -0500

    wip(config): se agrega configs

---
+++ src/initializers/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAdzQvfySGKxW-bFpi2KMnqYHK5BHhkvSE',
+  authDomain: 'albums-test-react.firebaseapp.com',
+  databaseURL: 'https://albums-test-react.firebaseio.com',
+  projectId: 'albums-test-react',
+  storageBucket: 'albums-test-react.appspot.com',
+  messagingSenderId: '148538828305'
+};
